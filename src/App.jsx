6270e124f69d43523ee539b80c329f37d3319363
Smrity11 <kanisasia376@gmail.com>
2023-09-15 05:18:06 -0700

    add remaining hour and total credit and total price showing right side

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unknown-property */
 import Cards from "./components/coursesCards/cards";
 import AddedCourse from "./components/chooseCourse/addedCourse";
 import { useState } from "react";
@@ -5,12 +6,36 @@ import { useState } from "react";
 import './App.css'
 
 function App() {
-  const [selectCourse ,setSelectCourse] =useState([])
-  const eventHandler =(course) =>{
-      const newSetvalue = [...selectCourse ,course]
-      setSelectCourse(newSetvalue)
-      console.log(course);
-     }
+  const [selectCourse, setSelectCourse] = useState([]);
+  const [totalCredit ,setTotalcredit] =useState(0)
+  const [totalRemaining ,setTotalRemaining] = useState(20)
+  const [totalPrice,setTotalPrice] =useState(0)
+
+const eventHandler = (course) => {
+  const isExist = selectCourse.find((item) => item.id === course.id);
+  let count= course.credit
+  let price =course.price
+  if (isExist) {
+    alert('Course is already selected.');
+  } else {
+    selectCourse.forEach( item =>{
+      count += item.credit
+      price += item.price
+    })
+    const totalRemainig =20 - count
+    if(count > 20){
+      return alert('you have no credit')
+    }
+    else{
+      setTotalcredit(count)
+      setTotalRemaining(totalRemainig)
+      setTotalPrice(price)
+      const newSetValue = [...selectCourse, course];
+      setSelectCourse(newSetValue);
+    }
+   
+  }
+};
 
   return (
     <>
@@ -19,7 +44,7 @@ function App() {
      <div className="flex flex-wrap md:flex-nowrap px-6 md:px-[50px] gap-8">
      <Cards eventHandler={eventHandler}></Cards>
      <div>
-            <AddedCourse selectCourse={selectCourse}></AddedCourse>
+            <AddedCourse totalPrice={totalPrice} totalRemaining={totalRemaining} totalCredit={totalCredit} selectCourse={selectCourse}></AddedCourse>
       </div>
       </div>
     </>
--- src/components/chooseCourse/addedCourse.jsx
+++ src/components/chooseCourse/addedCourse.jsx
@@ -1,20 +1,24 @@
 /* eslint-disable react/no-unknown-property */
 
 
-const AddedCourse = ({selectCourse}) => {
-    
+const AddedCourse = ({selectCourse,totalCredit,totalRemaining,totalPrice}) => {
+
     return (
         <>
         <div >
+       
            <div class="card bg-base-100 shadow-xl p-8 lg:w-[280px]">
-           <p>{selectCourse.length}</p>
-            <h4>Credit Hour Remaining 7 hr</h4>
+            <h4 className="text-[16px] text-sky-500 my-3 font-bold">Credit Hour Remaining {totalRemaining} hr</h4>
             <hr></hr>
-            <p className="text-black text-2xl">Course Name</p>
+            <p className="text-black text-2xl my-3">Course Name</p>
+            {
+                selectCourse.map(item => <li  key={item.id}>{item.title}
+                 </li>) 
+            }
             <hr></hr>
-            <p className="text-slate-500">Total Credit Hour : 13</p>
+            <p className="text-slate-500 my-3">Total Credit Hour : {totalCredit}</p>
             <hr></hr>
-            <p>Total Price : 48000 USD</p>
+            <p className="my-3">Total Price : {totalPrice} USD</p>
            </div>
         </div>
         </>
